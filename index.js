f59dc7101e805b14d99be4129744b6b78dab2fbd
JohnMutemi <jmtutorsalp@gmail.com>
2024-03-13 23:02:08 +0300

    modified js file

---
+++ index.js
@@ -0,0 +1,52 @@
+let number = 10;
+
+let a = 5;
+let b = 6;
+
+function add() {
+    "something";
+}
+
+function subtract() {
+    "something";
+}
+
+function multiply() {
+    "something";
+}
+
+function divide() {
+    "something";
+}
+
+function add(a,b) {
+  return (a+b);
+}
+
+function subtract(a,b) {
+    return (a-b);
+}
+
+function multiply(a,b) {
+    return (a*b);
+}
+
+function divide(a,b) {
+  return (a/b);
+}
+
+function increment(n) {
+    return n += 1;
+}
+
+function decrement(n) {
+    return n -= 1;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
